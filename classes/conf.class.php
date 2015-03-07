a883651b8b48e1cee713351d6b75763b1ede7752
Mako <makoks@gmail.com>
2015-03-07 16:27:38 -0600

    Actualizar config

--- classes/conf.class.php
+++ classes/conf.class.php
@@ -1,10 +1,10 @@
 <?php
 class Conf{
 	public static $domain  = "";
-	public static $db_host = "localhost";
-	public static $db_user = "root";
-	public static $db_pass = "";
-	public static $db_name = "taxi";
+	public static $db_host = "127.5.183.130";
+	public static $db_user = "adminMPbazUV";
+	public static $db_pass = "D_91S3afuq93";
+	public static $db_name = "taximap";
 	public static $time_zone = "America/Mexico_City";
 }
 date_default_timezone_set(Conf::$time_zone);
